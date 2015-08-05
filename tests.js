57a5ab1c1c78ea4334e8e1f560b1ffb3155d6730
Austin Cheney <info@prettydiff.com>
2015-08-04 23:59:12 -0500

    Updated code

--- tests.js
+++ tests.js
@@ -23,19 +23,37 @@ var tests = [
         units: [
             {
                 check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"beautify\"\n",
-                name: "Node command line input with readmethod screen. Beautify markup.",
+                name: "Base. Beautify markup.",
                 verify: "<a>\n    <b>\n        <c/>\n    </b>\n</a>"
             }, {
                 check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"minify\"\n",
-                name: "Node command line input with readmethod screen. Minify markup.",
+                name: "Base. Minify markup.",
                 verify: "<a><b> <c/> </b></a>"
             }, {
                 check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"parse\"\n",
-                name: "Node command line input with readmethod screen. Parse markup.",
+                name: "Base. Parse markup.",
                 verify: "{\"token\":[\"<a>\",\"<b>\",\" \",\"<c/>\",\" \",\"</b>\",\"</a>\"],\"types\":[\"start\",\"start\",\"content\",\"singleton\",\"content\",\"end\",\"end\"]}"
             }, {
                 group: "recursion test",
                 units: [
+                    {
+                        group: "extra recursion test",
+                        units: [
+                            {
+                                check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"beautify\"\n",
+                                name: "Extra Recursion Beautify markup.",
+                                verify: "<a>\n    <b>\n        <c/>\n    </b>\n</a>"
+                            }, {
+                                check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"minify\"\n",
+                                name: "Extra Recursion Minify markup.",
+                                verify: "<a><b> <c/> </b></a>"
+                            }, {
+                                check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"parse\"\n",
+                                name: "Extra Recursion Parse markup.",
+                                verify: "{\"token\":[\"<a>\",\"<b>\",\" \",\"<c/>\",\" \",\"</b>\",\"</a>\"],\"types\":[\"start\",\"start\",\"content\",\"singleton\",\"content\",\"end\",\"end\"]}"
+                            }
+                        ]
+                    },
                     {
                         check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"beautify\"\n",
                         name: "Recursion Beautify markup.",
@@ -48,28 +66,39 @@ var tests = [
                         check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"parse\"\n",
                         name: "Recursion Parse markup.",
                         verify: "{\"token\":[\"<a>\",\"<b>\",\" \",\"<c/>\",\" \",\"</b>\",\"</a>\"],\"types\":[\"start\",\"start\",\"content\",\"singleton\",\"content\",\"end\",\"end\"]}"
-                    }, {
-                        group: "extra recursion test",
-                        units: [
-                            {
-                                check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"beautify\"\n",
-                                name: "Recursion Beautify markup.",
-                                verify: "<a>\n    <b>\n        <c/>\n    </b>\n</a>"
-                            }, {
-                                check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"minify\"\n",
-                                name: "Recursion Minify markup.",
-                                verify: "<a><b> <c/> </b></a>"
-                            }, {
-                                check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"parse\"\n",
-                                name: "Recursion Parse markup.",
-                                verify: "{\"token\":[\"<a>\",\"<b>\",\" \",\"<c/>\",\" \",\"</b>\",\"</a>\"],\"types\":[\"start\",\"start\",\"content\",\"singleton\",\"content\",\"end\",\"end\"]}"
-                            }
-                        ]
                     }
                 ]
             }
         ]
     }
 ];
+
+/*var tests = [
+    {
+        group: "node-local",
+        //buildup: ["rm -rf node_modules","mkdir node_modules","npm install prettydiff"],
+        //teardown: ["rm -rf node_modules"],
+        units: [
+            {
+                group: "readmethod screen",
+                units: [
+                    {
+                        check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"beautify\"\n",
+                        name: "Node command line input with readmethod screen. Beautify markup.",
+                        verify: "<a>\n    <b>\n        <c/>\n    </b>\n</a>"
+                    }, {
+                        check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"minify\"\n",
+                        name: "Node command line input with readmethod screen. Minify markup.",
+                        verify: "<a><b> <c/> </b></a>"
+                    }, {
+                        check: "node node_modules/prettydiff/api/node-local.js source:\"<a><b> <c/>    </b></a>\" readmethod:\"screen\" mode:\"parse\"\n",
+                        name: "Node command line input with readmethod screen. Parse markup.",
+                        verify: "{\"token\":[\"<a>\",\"<b>\",\" \",\"<c/>\",\" \",\"</b>\",\"</a>\"],\"types\":[\"start\",\"start\",\"content\",\"singleton\",\"content\",\"end\",\"end\"]}"
+                    }
+                ]
+            }
+        ]
+    }
+];*/
 
 exports.tests = tests;
